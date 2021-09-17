2a3ba2a334cca54fa1135b34c5d783c08897d2e0
Liana03 <90089333+Liana03@users.noreply.github.com>
2021-09-17 06:54:17 +0400

    Add files via upload

---
+++ Homework 4 Liana Grigoryan.py
@@ -0,0 +1,32 @@
+"""LOOPS"""
+import re
+# 1. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a nested loop.
+# Օգտագործելով ցիկլեր, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
+#for i in range(1, 10):
+    #for j in range(1, 10):
+        #print('{:>2} * {} = {:>2}'.format(i, j, i**j), end='\t')
+    #print()
+# 2. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a list comprehension.
+# Օգտագործելով comprehension, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
+
+# 4. Count the number of 'b's in the given string. DO NOT use the count() method.
+# Հաշվել տրված սթրինգում 'b'-երի քանակը։ Չօգտագործել count() մեթոդը։
+nonsense = 'Blinking blindly, brainy Bob brings beautiful beer bottles beneath blue butterflies billowing brilliantly.'
+
+nonsense = "Mary had a little lamb"
+len(re.findall("b", nonsense))
+
+
+
+# 5. Write a program that will print the factorial of n.
+# Գրել ծրագիր, որը կտպի n թվի ֆակտորիալը։
+num = int(input("Enter a number: "))
+factorial = 1
+if num < 0:
+   print(" Factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
